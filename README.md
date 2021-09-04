d926568cefb410cf73a3272fee38474323c8bf10
a-r-t <alexthimineur@gmail.com>
2021-09-03 20:38:41 -0400

    update swing shapes, readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Brick Breaker
+
+This is a very simple Brick Breaker game.
+
+Shape classes in the `SwingShapes` package came from here: [https://github.com/SER120L/SwingShapes](https://github.com/SER120L/SwingShapes).
+
--- src/SwingShapes/Rectangle.java
+++ src/SwingShapes/Rectangle.java
@@ -2,7 +2,7 @@ package SwingShapes;
 
 import java.awt.Color;
 
-public class Rectangle extends SmartRectangularShape {
+public class Rectangle extends RectangleShape {
 	public Rectangle() {
 		super(new java.awt.geom.Rectangle2D.Double());
 	}
